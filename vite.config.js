100c10d61d716aeac99c9e963f677848af44505e
yoni7emil <yonathanemilioc@gmail.com>
2026-07-01 21:42:15 -0300

    Fix base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/E-Commer-RESORD/", //  nombre exacto de tu repo en GitHub
+  base: "/E-Commer-RESORD/", //  nombre exacto de repo en GitHub
 })
