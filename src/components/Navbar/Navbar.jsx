8d17287c829e46b440b93bebe1f9f3eb0c85adf2
yoni7emil <yonathanemilio@gmail.com>
2026-07-18 16:14:35 -0300

    Solucionando conflicto de merge

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,30 @@
 import { NavLink } from 'react-router-dom';
-import logo from '../../assets/IMG_1359.PNG'; // Asegúrate que el nombre sea exacto
+import logo from '../../assets/IMG_1359-r.PNG';
 import './Navbar.css'; // nuevo estilo
 
 //EN este componente, se recibe el carrito y la función setTerminoBusqueda como props.
 const Navbar = ({ carrito, setTerminoBusqueda }) => {
   return (
     <nav className="navbar-custom">
-      <div className="container">
-        <NavLink className="navbar-brand" to="/">
+      {/* Sección Izquierda: Buscador */}
+      <div className="nav-section left">
+        <input className="search-bar" type="search" placeholder="Buscar..." onChange={(e) => setTerminoBusqueda(e.target.value)} />
+      </div>
+
+      {/* Sección Centro: Logo */}
+      <div className="nav-section center">
+        <NavLink to="/">
           <img src={logo} alt="Logo" className="navbar-logo" />
         </NavLink>
-        {/* Aquí se agregan los enlaces de navegación y la barra de búsqueda */}
-        <div className="navbar-links">
-          <NavLink className="nav-link" to="/catalogo">Catálogo</NavLink>
-          <NavLink className="nav-link" to="/contacto">Contacto</NavLink>
-          <NavLink className="nav-link cart-link" to="/carrito">
-            Carrito ({carrito.length})
-          </NavLink>
-        </div>
-          {/* Barra de búsqueda */}
-        <input 
-          className="search-bar" 
-          type="search" 
-          placeholder="Buscar..." 
-          onChange={(e) => setTerminoBusqueda(e.target.value)}
-        />
+      </div>
+
+      {/* Sección Derecha: Links y Carrito */}
+      <div className="nav-section right">
+        <NavLink className="nav-link" to="/catalogo">Catálogo</NavLink>
+        <NavLink className="nav-link" to="/contacto">Contacto</NavLink>
+        <NavLink className="nav-link cart-link" to="/carrito">
+          Carrito ({carrito.length})
+        </NavLink>
       </div>
     </nav>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
-import logo from '../../assets/IMG_1359.PNG'; 
+import logo from '../../assets/IMG_1359-r.PNG'; 
 
 const Home = () => {
   return (
